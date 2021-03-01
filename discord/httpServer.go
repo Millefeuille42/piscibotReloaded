9e4a70fc3405460e615065a2405c443d7dc6568f
Millefeuille42 <mlabouri@student.42.fr>
2021-03-01 13:34:51 +0100

    Fixed http server

--- discord/httpServer.go
+++ discord/httpServer.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Message struct {
-	Data    string `json:"data"`
+	Message string `json:"data"`
 	Channel string `json:"channel"`
 	Login   string `json:"login"`
 }
@@ -15,8 +15,7 @@ type Message struct {
 func sendHandler(w http.ResponseWriter, r *http.Request) {
 	var data []byte
 
-	switch r.Method {
-	case "POST":
+	if r.Method == "POST" {
 		_, err := r.Body.Read(data)
 		if err != nil {
 			w.WriteHeader(500)
