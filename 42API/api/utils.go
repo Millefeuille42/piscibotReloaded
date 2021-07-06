5d39fa580db7ea0805e58906112126143398873f
Millefeuille <mlabouri@student.42.fr>
2021-07-06 16:24:46 +0200

    Added some utils for db to local transition

--- 42API/api/utils.go
+++ 42API/api/utils.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"runtime/debug"
 
 	apiclient "github.com/BoyerDamien/42APIClient"
 	mw "github.com/BoyerDamien/mongodbWrapper"
@@ -40,4 +42,58 @@ func CheckEnvVariables() {
 		}
 		log.Printf("%s env variable [OK]", val)
 	}
+}
+
+// Find Check if val exists in Slice, true if it exists
+func Find(slice []string, val string) bool {
+	for _, item := range slice {
+		if item == val {
+			return true
+		}
+	}
+	return false
+}
+
+// logError Prints error + StackTrace to stderr if error
+func logError(err error) {
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err, string(debug.Stack()))
+	}
+}
+
+// checkError Panic if error
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+// createDirIfNotExist Check if dir exists, if not create it
+func createDirIfNotExist(path string) error {
+	_, err := os.Stat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			err = os.Mkdir(path, os.ModePerm)
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+		return err
+	}
+	return nil
+}
+
+// createFileIfNotExist Check if file exists, if not create it
+func createFileIfNotExist(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		_, err := os.Create(path)
+		if err != nil {
+			logError(err)
+			return false, err
+		}
+		return false, nil
+	}
+	return true, nil
 }
