7953cd9da11b716040cff06d877c2d2b7911400b
Millefeuille <mlabouri@student.42.fr>
2021-07-06 17:04:47 +0200

    Useless utils cleanup

--- 42API/api/utils.go
+++ 42API/api/utils.go
@@ -9,31 +9,7 @@ import (
 	"runtime/debug"
 
 	apiclient "github.com/BoyerDamien/42APIClient"
-	mw "github.com/BoyerDamien/mongodbWrapper"
-	"go.mongodb.org/mongo-driver/bson"
-
-	"github.com/gofiber/fiber/v2"
 )
-
-// Exec runs endpoint functions
-func Exec(c *fiber.Ctx, f func(mw.Database, *fiber.Ctx) error) error {
-	if err := Wrapper.Init(MongoURL, DBCredentials); err != nil {
-		log.Println(err.Error())
-	}
-	defer Wrapper.Close()
-	db := Wrapper.GetDatabase(DatabaseName)
-	db.AddCollections(DatabaseName)
-	return f(db, c)
-}
-
-// IsExists checks if a user already exists in data base
-func IsExists(db mw.Database, user *apiclient.User) bool {
-	res, err := db.FindOne(DatabaseName, bson.M{"login": user.Login})
-	if err != nil || res.Err() != nil {
-		return false
-	}
-	return true
-}
 
 // CheckEnvVariables tests the existence of required env variables
 func CheckEnvVariables() {
@@ -46,27 +22,10 @@ func CheckEnvVariables() {
 	}
 }
 
-// Find Check if val exists in Slice, true if it exists
-func Find(slice []string, val string) bool {
-	for _, item := range slice {
-		if item == val {
-			return true
-		}
-	}
-	return false
-}
-
 // logError Prints error + StackTrace to stderr if error
 func logError(err error) {
 	if err != nil {
 		_, _ = fmt.Fprintln(os.Stderr, err, string(debug.Stack()))
-	}
-}
-
-// checkError Panic if error
-func checkError(err error) {
-	if err != nil {
-		panic(err)
 	}
 }
 
