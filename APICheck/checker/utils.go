01f8c4d25cb32d5b17a2417b5ca4b3e660aa2bae
Millefeuille42 <mathieu.labourier@gmail.com>
2021-10-11 16:43:58 +0200

    Fixed some deepSource issues

--- APICheck/checker/utils.go
+++ APICheck/checker/utils.go
@@ -15,7 +15,7 @@ func CheckProjectStatus(username string, p1, p2 *Project) error {
 		return fmt.Errorf("%s a raté le projet %s à %2.f%%! Boooh!", username, p2.Name, p2.FinalMark)
 	}
 	if p1.FinalMark != p2.FinalMark && p1.FinalMark == -42 {
-		return fmt.Errorf("@everyone %s a triché au projet %s! Quelle honte!", username, p2.Name, p2.FinalMark)
+		return fmt.Errorf("@everyone %s a triché au projet %s! Quelle honte!", username, p2.Name)
 	}
 	return nil
 }
